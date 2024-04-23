53d9e558f2e0032c42112d7503230d398de100ba
Sam Paugel <shpaugel@madisoncollege.edu>
2024-04-23 00:19:06 -0500

    Updated old code to current vrs
    Better vrs of Darkmode added and cleaned up old code

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,49 +1,34 @@
-// // Query for the toggle that is used to change between themes
-// const toggle = document.querySelector('#themeToggle');
-
-// // Listen for the toggle check/uncheck to toggle the dark class on the <body>
-// toggle.addEventListener('ionChange', (ev) => {
-//   document.body.classList.toggle('dark', ev.detail.checked);
-// });
-
-// const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-// // Listen for changes to the prefers-color-scheme media query
-// prefersDark.addListener((e) => checkToggle(e.matches));
-
-// // Called when the app loads
-// const loadApp = () => {
-//   checkToggle(prefersDark.matches);
-// }
-
-// // Called by the media query to check/uncheck the toggle
-// const checkToggle = (shouldCheck) => {
-//   toggle.checked = shouldCheck;
-// }
-
 import { useState, useEffect } from 'react';
-import "./darkMode.css";
+import { 
+    IonButton,
+    IonIcon
+} from "@ionic/react";
+import { invertModeOutline } from "ionicons/icons";
+import "../Master.css";
 
 export const DarkMode = () => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const toggleTheme = () => theme == 'light' ? setTheme('dark') : setTheme('light');    
     
-    useEffect(() => {
+  useEffect(
+ () => {
+        localStorage.setItem('theme', theme);
         document.body.className = theme;
-    }, [theme]);
-
-
-    const toggleTheme = () => {
-        if (theme === 'light') {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
-    };
-
+        }, [theme]);
 
     return (
-        <div className={`App ${theme}`}>
-            <button onClick={toggleTheme}>Dark Mode</button>
+    <>
+        <div id="darkModeToggleButton" >
+            <IonButton size="small" color="medium" 
+                className={`${theme}`} 
+                onClick={toggleTheme}
+            >
+            <IonIcon icon={invertModeOutline}></IonIcon>
+                Toggle Dark Mode
+            </IonButton>
         </div>
+    </>
+  
     );
 }
+
